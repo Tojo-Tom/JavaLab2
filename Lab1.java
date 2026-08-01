39a1fd7dc741db5787c16f0e500f43ef056b7aad
Tojo-Tom <tomjoy9074@gmail.com>
2026-08-01 10:00:00 +0530

    Add Lab1

---
+++ Lab1.java
@@ -0,0 +1,10 @@
+public class Lab1 {
+    static class Student {
+        Student() {
+            System.out.println("Welcome to Student Class");
+        }
+    }
+    public static void main(String[] args) {
+        new Student();
+    }
+}
